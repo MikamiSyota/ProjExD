fe2ad1db79e600ced03febcf500a383b97406deb
c0a21115b2 <c0a21115b2@edu.teu.ac.jp>
2022-11-29 14:45:28 +0900

    練習3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    i = btn["text"]
+    tkm.showinfo(i, f"[{i}]ボタンがクリックされました")
 
 root = tk.Tk()
 root.title("電卓")
@@ -9,6 +15,8 @@ c = 0
 for i in range(9, -1, -1):
     button = tk.Button(root, text=f"{i}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    button.grid()
     c += 1
     if c % 3 == 0:
         r += 1

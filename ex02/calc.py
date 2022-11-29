f102b3d43eb24bfbaf1c9a170469e767019e3c8b
c0a21115b2 <c0a21115b2@edu.teu.ac.jp>
2022-11-29 16:49:40 +0900

    進数変換

--- ex02/calc.py
+++ ex02/calc.py
@@ -18,11 +18,28 @@ def button_click(event):
     elif i == "÷":
         i = "/"
         entry.insert(tk.END, i)
-    # elif i == "②":
-    #     siki = entry.get() # 数式の文字列
-    #     if type(siki) == str:
-    #         entry.delete(0, tk.END) # 表示文字列の削除
-    #     entry.insert(tk.END, bin(siki)) # 結果の挿入
+    elif i == "②":
+        siki = entry.get() # 数式の文字列
+        if type(siki) == str:
+            entry.delete(0, tk.END) # 表示文字列の削除
+            entry.insert(tk.END, bin(int(siki))) # 結果の挿入
+        else:
+            entry.insert(tk.END, i)
+    elif i == "⑧":
+        siki = entry.get() # 数式の文字列
+        if type(siki) == str:
+            entry.delete(0, tk.END) # 表示文字列の削除
+            entry.insert(tk.END, oct(int(siki))) # 結果の挿入
+        else:
+            entry.insert(tk.END, i)
+    elif i == "⑯":
+        siki = entry.get() # 数式の文字列
+        if type(siki) == str:
+            entry.delete(0, tk.END) # 表示文字列の削除
+            entry.insert(tk.END, hex(int(siki))) # 結果の挿入
+        else:
+            entry.insert(tk.END, i)
+
     else:
         entry.insert(tk.END, i)
 
@@ -35,7 +52,7 @@ entry.grid(row=0, column=0, columnspan=4)
 
 r = 1
 c = 0
-for i in [7, 8, 9, "÷", 4, 5, 6, "x", 1, 2, 3, "-", "c", 0, "=", "+", "②", "⑧", "⑯"]:
+for i in [7, 8, 9, "÷", 4, 5, 6, "x", 1, 2, 3, "-", "c", 0, "=", "+", "②", "⑧", "⑯", " "]:
     button = tk.Button(root, text=f"{i}", width=4, height=2, font=("", 20))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)

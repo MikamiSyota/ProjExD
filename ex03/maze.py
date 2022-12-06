690dc28dfe955c4d980fd4112c24bd56a107ece0
c0a21115b2 <c0a21115b2@edu.teu.ac.jp>
2022-12-06 15:23:38 +0900

    練習問題12

--- ex03/maze.py
+++ ex03/maze.py
@@ -15,6 +15,11 @@ def main_proc():
     if key == "Down": my += 1
     if key == "Left": mx -= 1
     if key == "Right": mx += 1
+    if maze_lst[mx][my] == 1: #移動先が壁だったら
+        if key == "Up": my += 1
+        if key == "Down": my -= 1
+        if key == "Left": mx += 1
+        if key == "Right": mx -= 1
     cx = mx*100+50
     cy = my*100+50
     canvas.coords("kokaton", cx, cy)

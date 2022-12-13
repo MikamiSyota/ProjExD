50b891f49c32c89de5de845f7f91cf2b06b954be
c0a21115b2 <c0a21115b2@edu.teu.ac.jp>
2022-12-13 14:20:09 +0900

    練習問題3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,6 +9,13 @@ def main():
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
+    # 3
+    tori_sfc = pg.image.load("fig/2.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+    scrn_sfc.blit(tori_sfc, tori_rct)
+
     # 2
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct)
